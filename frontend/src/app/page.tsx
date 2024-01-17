2829d7e94d037863d3df187bb516dc35ca067a79
Jaegar Saunders <jaegarsaunders@192.168.219.9>
2024-01-17 12:07:53 -0330

    Setup custom color theme with tailwindCss

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1>Some day...</h1>
+      <h1 className="text-primaryBlue">Some day...</h1>
     </main>
   )
 }
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -14,6 +14,16 @@ const config: Config = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
+    colors: {
+      'primaryBlue': '#8BB9FE',
+      'primaryDark': '#11151B',
+      'secondaryDark': '#151A21',
+      'tetraDark': '#0E1F39', // Not sure if this is actually used in design yet
+      'quadraDark': '#1D304B', // Not sure if this is actually used in design yet
+      'primaryLight': '#B4BFD2',
+      'secondaryLight': '#737276',
+      
+    },
   },
   plugins: [],
 }
