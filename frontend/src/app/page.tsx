acd6c2cabc0f95333a2ca2843a9abdd2ba5b7944
Jaegar Saunders <jaegarsaunders@192.168.219.9>
2024-01-17 14:28:16 -0330

    Homepage layout

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <Navbar />
+      <section className='flex justify-center items-center h'></section>
     </main>
   )
 }
