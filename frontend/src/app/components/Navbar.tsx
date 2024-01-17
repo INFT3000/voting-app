da3285ef6f7cd6e7239e6a5de6de05757964a1ef
Sebastian Dunne <git@sebastian-dunne.com>
2024-01-17 10:17:54 -0800

    Convert logo to svg (#7)
    * svg & no ds store
    
    * pathify text in logo

--- frontend/src/app/components/Navbar.tsx
+++ frontend/src/app/components/Navbar.tsx
@@ -4,7 +4,7 @@ export default function Navbar() {
     return (
         <nav className='flex fixed top-0 left-0 right-0 px-[300px] py-[18px] justify-between items-center'>
             <div className="logo">
-                <Image src="/assets/logo-white.png" alt="logo" width={142} height={142} />
+                <Image src="/assets/logo-white.svg" alt="logo" width={142} height={142} />
             </div>
             <div className="links w-32">
                 <ul className='flex w-200 justify-between'>
