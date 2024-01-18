29108ffa985aa5c8af3c643e70a24d45e8e4afdc
Chance <w0480132@nscc.ca>
2024-01-18 13:38:16 -0400

    Remove Option Button

--- frontend/src/app/components/CreatePollWidget.tsx
+++ frontend/src/app/components/CreatePollWidget.tsx
@@ -3,6 +3,7 @@ import React from "react"
 import PollContainer from "./PollContainer"
 import Button from "./Button"
 import { useState } from "react";
+import { When } from "react-if";
 export default function CreatePollWidget() {
     const [options, setOptions] = useState(["", ""]);
 
@@ -18,26 +19,46 @@ export default function CreatePollWidget() {
         newOptions[index] = value;
         setOptions(newOptions);
     };
+
+    const handleOptionRemove = (index) => {
+        // Remove the specific option from the options array
+        const newOptions = [...options];
+        newOptions.splice(index, 1);
+        setOptions(newOptions);
+    };
     
     return (
         <PollContainer> 
             <form className="w-[100%] ">
                 <div className="inputGroup flex flex-col">
                     <label htmlFor="title" className="font-medium text-white mb-[5px]">Title</label>
-                    <input type="text" name="title" id="title" placeholder="Type your question here." className="bg-tetraDark rounded-lg p-[10px] mb-[10px] outline-none focus:border-primaryBlue focus:border-[1px] "/>
+                    <input type="text" name="title" id="title" placeholder="Type your question here." className="bg-tetraDark rounded-lg p-[10px] mb-[10px] outline-none focus:border-primaryBlue focus:border-[1px] text-white"/>
                 </div>
                 <div className="inputGroup flex flex-col">
                     <div className="inputGroup flex flex-col">
                         <fieldset className="flex flex-col">
                             <legend className="font-medium text-white mb-[5px]">Answer Options</legend>
                             {options.map((option, index) => (
-                                <input
+                                <div
                                     key={index}
+                                >
+                                <input
                                     placeholder={`Option ${index + 1}`}
                                     value={option}
                                     onChange={(e) => handleOptionChange(e.target.value, index)}
-                                    className="bg-tetraDark rounded-lg p-[10px] mb-[10px] outline-none focus:border-primaryBlue focus:border-[1px]"
+                                    className="bg-tetraDark rounded-lg p-[10px] mb-[10px] outline-none focus:border-primaryBlue focus:border-[1px] text-white"
                                 />
+                                {/*Only show the remove button if there are more than 2 options*/}
+                                <When condition={index > 1}>
+                                    <Button
+                                        text="x"
+                                        buttonType="button"
+                                        theme="secondary"
+                                        onClick={() => handleOptionRemove(index)}
+                                        className="w-[10%] h-[40px] text-[14px] px-[5px] hover:bg-primaryBlue hover:text-primaryDark transition-all"
+                                    />
+                                </When>
+                                </div>
                             ))}
                             <Button
                                 text="Add Option"
