bd8e51659a7cee166e02e64e262b9b58dc3b1462
Sebastian Dunne <git@sebastian-dunne.com>
2024-01-18 11:34:38 -0400

    add react-if

--- frontend/src/app/components/Button.tsx
+++ frontend/src/app/components/Button.tsx
@@ -1,7 +1,7 @@
 // import stuff from react-if
 
 import React from 'react'
-import { If, Then, Else, When, Unless, Switch, Case, Default } from 'react-if';
+import { If, Then, Else } from 'react-if';
 type ButtonProps = {
     text: string
     onClick?: () => void
@@ -40,31 +40,4 @@ export default function Button({ text, onClick, buttonType, styleType, href}: Bu
             </Else>
         </If>
     )
-
-
-    // Old switch statement left here just in case
-    // switch(buttonType) {
-    //     case 'button':
-    //         return styleType === 'primary' ? (
-    //             <button onClick={onClick} className="bg-primaryBlue rounded-lg text-primaryDark font-medium px-[50px] py-[7px]">
-    //                 {text}
-    //             </button>
-    //           ) : (
-    //             <button onClick={onClick} className="bg-transparent border-primaryBlue border-2 rounded-lg text-white font-medium px-[50px] py-[7px]">
-    //                 {text}
-    //             </button>
-    //           )
-    //     case 'anchor':
-    //         return styleType === 'primary' ? (
-    //             <a href={href} className="bg-primaryBlue rounded-lg text-primaryDark font-medium px-[50px] py-[7px]">{text}</a>
-    //           ) : (
-    //             <a href={href} className="bg-transparent border-primaryBlue border-2 rounded-lg text-white font-medium px-[50px] py-[7px]">{text}</a>
-    //           );
-              
-    //     default:
-    //         return (
-    //             <p className="text-white font-bold">Invalid button type property. Please use &quot;button&quot; or &quot;anchor&quot;</p>
-    //         )
-            
-    // }
 }
