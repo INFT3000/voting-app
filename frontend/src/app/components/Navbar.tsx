288c24e39acc947c268510e87f78f3ea0c02ecfe
Josh <113129413+BigBandzJosh@users.noreply.github.com>
2024-01-18 20:09:01 -0400

    Navbarbranch (#29)
    * Navbar is now responsive on mobile devices
    
    * Navbar is responsive on mobile

--- frontend/src/app/components/Navbar.tsx
+++ frontend/src/app/components/Navbar.tsx
@@ -1,22 +1,21 @@
+import Image from 'next/image';
 
-import Image from 'next/image'
 export default function Navbar() {
     return (
-        <nav className='flex fixed top-0 left-0 right-0 px-[300px] py-[18px] justify-between items-center'>
-            <div className="logo">
+        <nav className='flex fixed top-0 left-0 right-0 px-4 sm:px-6 md:px-8 py-4 justify-between items-center'>
+            <div className="logo p-2">
                 <Image src="/assets/logo-white.svg" alt="logo" width={142} height={142} />
             </div>
-            <div className="links w-32">
-                <ul className='flex w-200 justify-between'>
-                    <li className=''>
+            <div className="links sm:block w-32">
+                <ul className='flex space-x-4'>
+                    <li>
                         <a href="" className='text-white font-medium hover:underline hover:text-primaryBlue'>Login</a>
                     </li>
-                    <li className=''>
+                    <li>
                         <a href="" className='text-white font-medium hover:underline hover:text-primaryBlue'>Signup</a>
                     </li>
                 </ul>
             </div>
-
         </nav>
     );
 }
