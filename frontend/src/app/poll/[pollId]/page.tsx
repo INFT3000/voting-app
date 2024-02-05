77c14d502860300c2d084670872d5d1810428a8f
Jaegar Saunders <jaegarsaunders@192.168.219.9>
2024-02-05 16:44:34 -0330

    Axios request in dynamic poll segment page works

--- frontend/src/app/poll/[pollId]/page.tsx
+++ frontend/src/app/poll/[pollId]/page.tsx
@@ -7,21 +7,26 @@ type Poll = {
   uuid: string;
   title: string;
   options: string[];
+  settings: {
+    is_multiple_choice: boolean;
+    disallow_anonymous: boolean;
+    disallow_same_ip: boolean;
+  }
   // probably will want to add more fields like the votes to each options, im not sure
 };
 
 
 async function getPollByUuid(pollId: string) {
 
-const res = await axios.get(`http://localhost:8080/api/poll/${pollId}`);
-const poll: Poll = res.data;
-
-return poll;
+  const res = await axios.get(`http://localhost:8080/api/poll/${pollId}`);
+  const poll: Poll = res.data.poll;
+  return poll;
 }
 
 export default async function Page({params}) {
   const pollId = params.pollId;
   const poll = await getPollByUuid(pollId as string); 
+  console.log(poll.title);
   console.log(poll);
 
   return (
