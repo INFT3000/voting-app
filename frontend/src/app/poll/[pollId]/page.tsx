9277e5ff136fbc2354aed29274a2ef86dd24db6f
Jaegar Saunders <jaegarsaunders@192.168.219.9>
2024-02-05 16:04:38 -0330

    Setup the beggining of dynamic routes

--- frontend/src/app/poll/[pollId]/page.tsx
+++ frontend/src/app/poll/[pollId]/page.tsx
@@ -1,11 +1,32 @@
-
 import axios from 'axios';
 
+import { useRouter } from 'next/router';
 
-export default function PollPage({ poll }) {
+
+type Poll = {
+  uuid: string;
+  title: string;
+  options: string[];
+  // probably will want to add more fields like the votes to each options, im not sure
+};
+
+
+async function getPollByUuid(pollId: string) {
+
+const res = await axios.get(`http://localhost:8080/poll/${pollId}`);
+const poll: Poll = res.data;
+
+return poll;
+}
+
+export default async function Page({params}) {
+  const pollId = params.pollId;
+  const poll = await getPollByUuid(pollId as string); 
+  console.log(poll);
+
   return (
     <div>
-      <h1>poll</h1>
+      <h1>Poll: {poll.title}</h1>
       {/* Render your poll content here */}
     </div>
   );
