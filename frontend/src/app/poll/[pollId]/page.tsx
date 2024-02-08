9f6e5aabac09a99cd184315e2db2814007f79a02
Chance <w0480132@nscc.ca>
2024-02-08 15:39:54 -0400

    Vote casting progress

--- frontend/src/app/poll/[pollId]/page.tsx
+++ frontend/src/app/poll/[pollId]/page.tsx
@@ -1,3 +1,10 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+
+import Button from "@/app/components/Button";
+import Navbar from "@/app/components/Navbar";
+import PollContainer from "@/app/components/PollContainer";
 import { QpAxios } from "@/helpers/quickpollaxios";
 
 type Poll = {
@@ -11,21 +18,62 @@ type Poll = {
   }
 };
 
-async function getPollByUuid(pollId: string): Promise<Poll> {
-  const res = await QpAxios.get<{ poll: Poll }>(`poll/${pollId}`);
-  const { poll } = res.data;
+interface Option {
+  option: string;
+}
+
+function getPollByUuid(pollId: string): Poll {
+  // const res = await QpAxios.get<{ poll: Poll }>(`poll/${pollId}`);
+  // const { poll } = res.data;
+  const poll: Poll = {
+    uuid: "123",
+    title: "Title",
+    options: ["Option 1", "Option 2"],
+    settings: {
+      is_multiple_choice: true,
+      disallow_anonymous: false,
+      disallow_same_ip: false,
+    },
+  };
   return poll;
 }
 
-export default async function Page({ params }: { params: { pollId: string } }): Promise<JSX.Element> {
+export default function Page({ params }: { params: { pollId: string } }): JSX.Element {
   const { pollId } = params;
-  const poll = await getPollByUuid(pollId as string);
+  const poll = getPollByUuid(pollId as string);
+  const { register, handleSubmit } = useForm<Option>();
   console.log(poll);
 
+  const onSubmit = async (data: Option): Promise<void> => {
+    console.log(data);
+  };
+
   return (
-    <div>
-      <h1>Poll: {poll.title}</h1>
-      {/* Render your poll content here */}
-    </div>
+    <main className="flex min-h-screen flex-col justify-center">
+      <Navbar />
+      <PollContainer>
+        <form className="flex w-[100%] flex-col justify-start gap-[45px]" onSubmit={handleSubmit(onSubmit)}>
+          <div>
+            <h1 className="text-3xl font-bold">{poll.title}</h1>
+            <p className="text-secondaryGrey">By Anonymous</p>
+          </div>
+          <div className="flex flex-col gap-[20px]">
+            <p>Make a choice:</p>
+            <div>
+              {poll.options.map((option) => (
+                <div className="mb-3 flex gap-[12px] text-secondaryGrey" key={option}>
+                  <input type={poll.settings.is_multiple_choice ? "checkbox" : "radio"} id={option} name="option" value={option} />
+                  <label htmlFor={option}>{option}</label>
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className="flex gap-[18px]">
+            <Button theme="primary" type="submit">Vote</Button>
+            <Button theme="secondary">Results</Button>
+          </div>
+        </form>
+      </PollContainer>
+    </main>
   );
 }
