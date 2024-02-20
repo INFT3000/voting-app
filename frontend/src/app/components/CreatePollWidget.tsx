98f02c4c3b7541d8dc8fa2d8f0629057ebed8365
Sebastian Dunne <git@sebastian-dunne.com>
2024-02-20 02:41:57 -0400

    next/navigation router

--- frontend/src/app/components/CreatePollWidget.tsx
+++ frontend/src/app/components/CreatePollWidget.tsx
@@ -3,7 +3,7 @@
 import { ErrorMessage } from "@hookform/error-message";
 import axios from "axios";
 import Image from "next/image";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { When } from "react-if";
